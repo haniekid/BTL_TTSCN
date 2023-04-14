accf64f097877683b4b3c6c028fda0c342914b59
khainguyen2003 <quyet3582@gmail.com>
2023-04-14 17:34:35 +0700

    New changes

--- javascript/order.js
+++ javascript/order.js
@@ -1,12 +1,3 @@
-import {
-  order_hots_data,
-  instant_data,
-  order_fresh_fruit_data,
-  order_macchiato_cream_data,
-  order_milk_tea_data,
-  order_sua_chua_data,
-} from '../assets/data/product_data.js';
-
 const select = (el, all = false) => {
   el = el.trim();
   if (all) {
@@ -37,243 +28,148 @@ const onscroll = (el, listener) => {
   el.addEventListener('scroll', listener);
 };
 
-let order_container1 = select('.order-catogory1 .order-container');
-let order_container2 = select('.order-catogory2 .order-container');
-let order_container3 = select('.order-catogory3 .order-container');
-let order_container4 = select('.order-catogory4 .order-container');
-let order_container5 = select('.order-catogory5 .order-container');
-let order_container6 = select('.order-catogory6 .order-container');
-
-// ---------------------- Generate code start
-function init_order_hots_data() {
-  order_hots_data.forEach((product, key) => {
-    if (product.discount === product.price) {
-      product.price = '';
+let selectHeader = select('#order-header');
+if (selectHeader) {
+  const headerScrolled = () => {
+    if (window.scrollY > 100) {
+      selectHeader.classList.add('header-scrolled');
+    } else {
+      selectHeader.classList.remove('header-scrolled');
     }
-    let newDiv = document.createElement('div');
-    newDiv.className = 'product';
-    newDiv.innerHTML = `
-        <img
-          src=${product.image}
-          alt=""
-          class="product-thumb"
-        />
-        <div class="product-desc">
-          <h3 class="product-name">${product.title}</h3>
-          <span class="cost">${product.price}</span>
-          <span class="price-discount">${product.discount}</span>
-          <button class="btn-add-to-card" onclick="addToCard(${key}>Đặt hàng</button>
-        </div>
-    `;
-    order_container1.appendChild(newDiv);
-  });
+  };
+  window.addEventListener('load', headerScrolled);
+  onscroll(document, headerScrolled);
 }
-init_order_hots_data();
 
-function init_instant_data() {
-  instant_data.forEach((product, key) => {
-    if (product.discount === product.price) {
-      product.price = '';
+/**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = select('.order-catagories .scrollto', true);
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200;
+  navbarlinks.forEach((navbarlink) => {
+    if (!navbarlink.hash) return;
+    let section = select(navbarlink.hash);
+    if (!section) return;
+    if (
+      position >= section.offsetTop &&
+      position <= section.offsetTop + section.offsetHeight
+    ) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
     }
-    let newDiv = document.createElement('div');
-    newDiv.className = 'product';
-    newDiv.innerHTML = `
-        <img
-          src=${product.image}
-          alt=""
-          class="product-thumb"
-        />
-        <div class="product-desc">
-          <h3 class="product-name">${product.title}</h3>
-          <span class="cost">${product.price}</span>
-          <span class="price-discount">${product.discount}</span>
-          <button class="btn-add-to-card" onclick="addToCard(${key}>Đặt hàng</button>
-        </div>
-    `;
-    order_container2.appendChild(newDiv);
   });
-}
-init_instant_data();
+};
+window.addEventListener('load', navbarlinksActive);
+onscroll(window, navbarlinksActive);
 
-function init_order_fresh_fruit_data() {
-  order_fresh_fruit_data.forEach((product, key) => {
-    if (product.discount === product.price) {
-      product.price = '';
-    }
-    let newDiv = document.createElement('div');
-    newDiv.className = 'product';
-    newDiv.innerHTML = `
-        <img
-          src=${product.image}
-          alt=""
-          class="product-thumb"
-        />
-        <div class="product-desc">
-          <h3 class="product-name">${product.title}</h3>
-          <span class="cost">${product.price}</span>
-          <span class="price-discount">${product.discount}</span>
-          <button class="btn-add-to-card" onclick="addToCard(${key}>Đặt hàng</button>
-        </div>
-    `;
-    order_container3.appendChild(newDiv);
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let elementPos = select(el).offsetTop - 150;
+  window.scrollTo({
+    top: elementPos,
+    behavior: 'smooth',
   });
-}
-init_order_fresh_fruit_data();
+};
 
-function init_order_macchiato_cream_data() {
-  order_macchiato_cream_data.forEach((product, key) => {
-    if (product.discount === product.price) {
-      product.price = '';
-    }
-    let newDiv = document.createElement('div');
-    newDiv.className = 'product';
-    newDiv.innerHTML = `
-        <img
-          src=${product.image}
-          alt=""
-          class="product-thumb"
-        />
-        <div class="product-desc">
-          <h3 class="product-name">${product.title}</h3>
-          <span class="cost">${product.price}</span>
-          <span class="price-discount">${product.discount}</span>
-          <button class="btn-add-to-card" onclick="addToCard(${key}>Đặt hàng</button>
-        </div>
-    `;
-    order_container4.appendChild(newDiv);
-  });
-}
-init_order_macchiato_cream_data();
+/**
+ * Scrool with ofset on links with a class name .scrollto
+ */
+on(
+  'click',
+  '.scrollto',
+  function (e) {
+    if (select(this.hash)) {
+      e.preventDefault();
 
-function init_order_milk_tea_data() {
-  order_milk_tea_data.forEach((product, key) => {
-    if (product.discount === product.price) {
-      product.price = '';
-    }
-    let newDiv = document.createElement('div');
-    newDiv.className = 'product';
-    newDiv.innerHTML = `
-        <img
-          src=${product.image}
-          alt=""
-          class="product-thumb"
-        />
-        <div class="product-desc">
-          <h3 class="product-name">${product.title}</h3>
-          <span class="cost">${product.price}</span>
-          <span class="price-discount">${product.discount}</span>
-          <button class="btn-add-to-card" onclick="addToCard(${key}>Đặt hàng</button>
-        </div>
-    `;
-    order_container5.appendChild(newDiv);
-  });
-}
-init_order_milk_tea_data();
-function init6() {
-  order_sua_chua_data.forEach((product, key) => {
-    if (product.discount === product.price) {
-      product.price = '';
-    }
-    let newDiv = document.createElement('div');
-    newDiv.className = 'product';
-    newDiv.innerHTML = `
-        <img
-          src=${product.image}
-          alt=""
-          class="product-thumb"
-        />
-        <div class="product-desc">
-          <h3 class="product-name">${product.title}</h3>
-          <span class="cost">${product.price}</span>
-          <span class="price-discount">${product.discount}</span>
-          <button class="btn-add-to-card" onclick="addToCard(${key}>Đặt hàng</button>
-        </div>
-    `;
-    order_container6.appendChild(newDiv);
-  });
-}
-init6();
-/* ------------------ Generate code end ------------------ */
-
-document.addEventListener('DOMContentLoaded', () => {
-  let selectHeader = select('#order-header');
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled');
-      } else {
-        selectHeader.classList.remove('header-scrolled');
+      let body = select('body');
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active');
+        let navbarToggle = select('.mobile-nav-toggle');
+        navbarToggle.classList.toggle('bi-list');
+        navbarToggle.classList.toggle('bi-x');
       }
-    };
-    window.addEventListener('load', headerScrolled);
-    onscroll(document, headerScrolled);
+      scrollto(this.hash);
+    }
+  },
+  true
+);
+
+/**
+ * Scroll with ofset on page load with hash links in the url
+ */
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    if (select(window.location.hash)) {
+      scrollto(window.location.hash);
+    }
   }
+});
 
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('.order-catagories .scrollto', true);
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add('active');
-      } else {
-        navbarlink.classList.remove('active');
-      }
-    });
-  };
-  window.addEventListener('load', navbarlinksActive);
-  onscroll(window, navbarlinksActive);
+/**
+ * Xử lý các chức năng với giỏ hànghàng
+ */
+let listCards = [];
+let listCard = select('.listCard');
+let total = select('.total');
+let quantity = select('.quantity');
+let cart = select('#cart');
 
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let elementPos = select(el).offsetTop - 150;
-    window.scrollTo({
-      top: elementPos,
-      behavior: 'smooth',
-    });
-  };
+/**
+ * Chức năng ẩn hiện card khi click vào icon giỏ hàng
+ */
+on('click', '#card', () => {
+  card.classList.toggle('active');
+});
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on(
-    'click',
-    '.scrollto',
-    function (e) {
-      if (select(this.hash)) {
-        e.preventDefault();
-
-        let body = select('body');
-        if (body.classList.contains('mobile-nav-active')) {
-          body.classList.remove('mobile-nav-active');
-          let navbarToggle = select('.mobile-nav-toggle');
-          navbarToggle.classList.toggle('bi-list');
-          navbarToggle.classList.toggle('bi-x');
-        }
-        scrollto(this.hash);
-      }
-    },
-    true
-  );
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash);
-      }
+/**
+ * Chức năng thêm vào cart khi click vào nút add
+ */
+function addToCard(key, originArray) {
+  if (listCards[key] == null) {
+    // copy product form list to list card
+    listCards[key] = JSON.parse(JSON.stringify(originArray[key]));
+    listCards[key].quantity = 1;
+  }
+  reloadCard();
+}
+function reloadCard() {
+  listCard.innerHTML = '';
+  let count = 0;
+  let totalPrice = 0;
+  listCards.forEach((value, key) => {
+    totalPrice = totalPrice + value.price;
+    count = count + value.quantity;
+    if (value != null) {
+      let newDiv = document.createElement('li');
+      newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity - 1
+      })">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity + 1
+      })">+</button>
+                </div>`;
+      listCard.appendChild(newDiv);
     }
   });
-});
+  total.innerText = totalPrice.toLocaleString();
+  quantity.innerText = count;
+}
+function changeQuantity(key, quantity) {
+  if (quantity == 0) {
+    delete listCards[key];
+  } else {
+    listCards[key].quantity = quantity;
+    listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
+}
---
+++ javascript/orderGenerate.js
@@ -0,0 +1,190 @@
+import {
+  order_hots_data,
+  instant_data,
+  order_fresh_fruit_data,
+  order_macchiato_cream_data,
+  order_milk_tea_data,
+  order_sua_chua_data,
+} from '../assets/data/product_data.js';
+
+let order_container1 = document.querySelector(
+  '.order-catogory1 .order-container'
+);
+let order_container2 = document.querySelector(
+  '.order-catogory2 .order-container'
+);
+let order_container3 = document.querySelector(
+  '.order-catogory3 .order-container'
+);
+let order_container4 = document.querySelector(
+  '.order-catogory4 .order-container'
+);
+let order_container5 = document.querySelector(
+  '.order-catogory5 .order-container'
+);
+let order_container6 = document.querySelector(
+  '.order-catogory6 .order-container'
+);
+
+// ---------------------- Generate code start
+function init_order_hots_data() {
+  order_hots_data.map((product, key, originArray) => {
+    if (product.discount === product.price) {
+      product.price = '';
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'product';
+    newDiv.innerHTML = `
+        <img
+          src=${product.image}
+          alt=""
+          class="product-thumb"
+        />
+        <div class="product-desc">
+          <h3 class="product-name">${product.title}</h3>
+          <div className="price">
+            <span class="price-discount">${product.discount}</span>
+            <span class="cost">${product.price}</span>
+          </div>
+          <button class="btn-add-to-card" onclick="addToCard(${key})">Đặt hàng</button>
+        </div>
+    `;
+    order_container1.appendChild(newDiv);
+  });
+}
+init_order_hots_data();
+
+function init_instant_data() {
+  instant_data.forEach((product, key) => {
+    if (product.discount === product.price) {
+      product.price = '';
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'product';
+    newDiv.innerHTML = `
+        <img
+          src=${product.image}
+          alt=""
+          class="product-thumb"
+        />
+        <div class="product-desc">
+          <h3 class="product-name">${product.title}</h3>
+          <div className="price">
+            <span class="price-discount">${product.discount}</span>
+            <span class="cost">${product.price}</span>
+          </div>
+          <button class="btn-add-to-card" onclick="addToCard(${key})">Đặt hàng</button>
+        </div>
+    `;
+    order_container2.appendChild(newDiv);
+  });
+}
+init_instant_data();
+
+function init_order_fresh_fruit_data() {
+  order_fresh_fruit_data.forEach((product, key) => {
+    if (product.discount === product.price) {
+      product.price = '';
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'product';
+    newDiv.innerHTML = `
+        <img
+          src=${product.image}
+          alt=""
+          class="product-thumb"
+        />
+        <div class="product-desc">
+          <h3 class="product-name">${product.title}</h3>
+          <div className="price">
+            <span class="price-discount">${product.discount}</span>
+            <span class="cost">${product.price}</span>
+          </div>
+          <button class="btn-add-to-card" onclick="addToCard(${key})">Đặt hàng</button>
+        </div>
+    `;
+    order_container3.appendChild(newDiv);
+  });
+}
+init_order_fresh_fruit_data();
+
+function init_order_macchiato_cream_data() {
+  order_macchiato_cream_data.forEach((product, key) => {
+    if (product.discount === product.price) {
+      product.price = '';
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'product';
+    newDiv.innerHTML = `
+        <img
+          src=${product.image}
+          alt=""
+          class="product-thumb"
+        />
+        <div class="product-desc">
+          <h3 class="product-name">${product.title}</h3>
+          <div className="price">
+            <span class="price-discount">${product.discount}</span>
+            <span class="cost">${product.price}</span>
+          </div>
+          <button class="btn-add-to-card" onclick="addToCard(${key})">Đặt hàng</button>
+        </div>
+    `;
+    order_container4.appendChild(newDiv);
+  });
+}
+init_order_macchiato_cream_data();
+
+function init_order_milk_tea_data() {
+  order_milk_tea_data.forEach((product, key) => {
+    if (product.discount === product.price) {
+      product.price = '';
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'product';
+    newDiv.innerHTML = `
+        <img
+          src=${product.image}
+          alt=""
+          class="product-thumb"
+        />
+        <div class="product-desc">
+          <h3 class="product-name">${product.title}</h3>
+          <div className="price">
+            <span class="price-discount">${product.discount}</span>
+            <span class="cost">${product.price}</span>
+          </div>
+          <button class="btn-add-to-card" onclick="addToCard(${key})">Đặt hàng</button>
+        </div>
+    `;
+    order_container5.appendChild(newDiv);
+  });
+}
+init_order_milk_tea_data();
+function init6() {
+  order_sua_chua_data.forEach((product, key) => {
+    if (product.discount === product.price) {
+      product.price = '';
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'product';
+    newDiv.innerHTML = `
+        <img
+          src=${product.image}
+          alt=""
+          class="product-thumb"
+        />
+        <div class="product-desc">
+          <h3 class="product-name">${product.title}</h3>
+          <div className="price">
+            <span class="price-discount">${product.discount}</span>
+            <span class="cost">${product.price}</span>
+          </div>
+          <button class="btn-add-to-card" onclick="addToCard(${key})">Đặt hàng</button>
+        </div>
+    `;
+    order_container6.appendChild(newDiv);
+  });
+}
+init6();
+/* ------------------ Generate code end ------------------ */
