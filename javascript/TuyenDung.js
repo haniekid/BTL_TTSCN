d780ac5e0f1d62545da704b2de73711d7dd5231e
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2023-04-21 13:44:40 +0700

    Xong trang TuyenDung.html

--- javascript/TuyenDung.js
+++ javascript/TuyenDung.js
@@ -1,20 +1,32 @@
 /**
  * Thay đổi giao diện
  */
+let isToggle = false;
 function update(){
     window.requestAnimationFrame(update);
     let carousel_inner = document.querySelector(".carousel-inner");
     let height_slide = document.querySelectorAll(".slide")[1].offsetHeight;
     carousel_inner.style.height = height_slide + "px";
     let nav = document.getElementById("navbar");
-    if(window.scrollY > 100){
+    if(window.scrollY > 100 || isToggle){
         nav.style.backgroundColor = "#212529";
     }else{
         nav.style.backgroundColor = "transparent";
     }
 }
 update();
-
+/**
+ * Navbar toggle
+ */
+let navbar = document.querySelector(".navbar");
+document.getElementById("navbar-toggle").addEventListener("click", () => {
+    isToggle = !isToggle;
+    if(isToggle){
+        navbar.classList.add("active");
+    }else{
+        navbar.classList.remove("active");
+    }
+});
 /**
  * slide show
  */
